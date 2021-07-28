78f6b72ccee4f8109e3e4138ad8107cd17d205e3
create with ansible <v.vumna@vinbigdata.org>
2021-07-28 17:00:25 +0700

    fix config path

--- spliceai.py
+++ spliceai.py
@@ -22,8 +22,11 @@ except ImportError:
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # FATAL
 #logging.getLogger('tensorflow').setLevel(logging.FATAL)
 import yaml
-
-with open(Path(resources.path(config_file,'spliceai_logging_config.yml')).absolute(), 'r') as config:
+logger_file = resources.path(config_file,'spliceai_logging_config.yml')
+logger_path = Path(logger_file).absolute()
+print(logger_path)
+logger_path = t.Union(logger_path,str)
+with open(logger_path, 'r') as config:
     logging.config.dictConfig(yaml.safe_load(config))
 
 logger = logging.getLogger('cloaked_chatter')
